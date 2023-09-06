62a5112725514cbc18f0aab8b2d3fec0703d88db
Nibha <nibhamaharjan@yahoo.com>
2023-09-06 11:45:39 -0400

    1st

--- App.js
+++ App.js
@@ -4,17 +4,25 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>About me</Text>
+      <Text>Hello, My name is Nibha Maharjan</Text>
+      <Text>I come from Kathmandu, Nepal</Text>
+      <Text>Hello, My name is Nibha Maharjan</Text>
+      <Text>I completed my Bsc(Hons)IT from London Metropolitan</Text>
+      <Text>University</Text>
       <StatusBar style="auto" />
-    </View>
+    </View> 
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding:20,
     flex: 1,
-    backgroundColor: '#fff',
+    fontSize: 45,
+    backgroundColor: '#b2ff7a',
     alignItems: 'center',
     justifyContent: 'center',
+    textAlign: 'center',
   },
 });
